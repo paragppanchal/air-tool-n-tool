8aa6805721fc0b6be8a035bf9c7d053d02414a69
Joy Seng <allthatjoy@gmail.com>
2018-08-14 15:18:14 +1000

    seeds added for tools

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require 'faker'
 
 puts 'creating 10 users'
+User.destroy_all
 
 10.times do
   user = User.new(
@@ -8,6 +9,23 @@ puts 'creating 10 users'
     password: Faker::Internet.password
     )
   user.save!
+end
+
+puts 'Finished!!!'
+
+all_users = User.all
+
+puts 'creating 10 tools'
+Tool.destroy_all
+
+10.times do
+  current_user = all_users.sample
+  tool = Tool.new(
+    title: Faker::Appliance.equipment,
+    price_per_day: Faker::Number.decimal(2)
+    )
+  tool.user = current_user
+  tool.save!
 end
 
 puts 'Finished!!!'
