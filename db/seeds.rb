045a1750d3ab5fd0ac79ac6b75ff2c233d28affb
Joy Seng <allthatjoy@gmail.com>
2018-08-14 14:51:29 +1000

    added seeds from faker for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'creating 10 users'
+
+10.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+    )
+  user.save!
+end
+
+puts 'Finished!!!'
