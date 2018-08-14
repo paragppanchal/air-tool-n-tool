a4f691a03a0f3d56516b1c658265d5df56b75fc8
Joy Seng <allthatjoy@gmail.com>
2018-08-14 11:59:25 +1000

    refactored tools-controller with before_action

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,35 +1,45 @@
 class ToolsController < ApplicationController
+
+  before_action: :set_tool, only: [:show, :edit, :update, :destroy]
+
   def index
     @tools = Tool.all
   end
+
   def show
-    @tool = Tool.find(params[:id])
   end
+
   def new
     @user = User.find(params[:user_id])
     @tool = Tool.new(user: @user)
   end
+
   def create
     @tool = Tool.new(tool_params)
     @user = User.find(params[:user_id])
     @tool.user = @user
     @tool.save
   end
+
   def edit
-    @tool = Tool.find(params[:id])
   end
+
   def update
-    @tool = Tool.find(params[:id])
     @tool.update(tool_params)
   end
+
   def destroy
-    @tool = Tool.find(params[:id])
     @tool.destroy
   end
 
   private
+
   def tool_params
     params.require(:tool).permit(:price_per_day)
+  end
+
+  def set_tool
+    @tool = Tool.find(params[:id])
   end
 
 end
