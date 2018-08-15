de5b1dfe410e7db7f51c9954a60fe257daf1f528
Parag Panchal <parag@relteksys.com.au>
2018-08-15 14:51:46 +1000

    adding of booking controller

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,48 @@
+class BookingPolicy < ApplicationPolicy
+ attr_reader :current_user
+
+  def initialize(current_user, tool)
+    @current_user = current_user
+    @tool = tool
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    @tool.user == @current_user
+  end
+
+  def destroy?
+    update?
+  end
+
+
+  class Scope
+
+    def initialize(current_user, scope)
+
+      @current_user = current_user
+      @scope = scope
+    end
+
+    def resolve
+      @scope.all
+    end
+  end
+
+
+end
